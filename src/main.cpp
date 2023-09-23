867dc709222bad2c30811e35b19e5116f6e45503
Shr1ftyy <syeamtechdemon@example.com>
2023-09-23 20:05:09 +0930

    ship movement + OOB + bullets

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,65 @@
 #include "raymath.h"
 #include <iostream>
 #include <fmt/core.h>
+#include <chrono>
+#include <vector>
+
+#define BULLET_HEIGHT 10
+#define BULLET_WIDTH 5
+#define BULLET_VEL 5
+#define BULLET_PER_SECOND 2
+
+//------------------------------------------------------------------------------------
+// fire weapons
+//------------------------------------------------------------------------------------
+void fireWeapons(Vector2 shipOrigin, int shipSize, std::vector<Rectangle*>* bullets) {
+    Rectangle* newBullet = new Rectangle;
+    newBullet->x = shipOrigin.x - BULLET_WIDTH / 2;
+    newBullet->y = shipOrigin.y - (shipSize / 2) - BULLET_HEIGHT;
+    newBullet->width = BULLET_WIDTH;
+    newBullet->height = BULLET_HEIGHT;
+    
+    bullets->push_back(newBullet);
+}
+
+//------------------------------------------------------------------------------------
+// Out-Of-Bounds check
+//------------------------------------------------------------------------------------
+bool outOfBounds(Vector2 entity, int screenWidth, int screenHeight) {
+    if (entity.x <= 0 || entity.x >= screenWidth
+        || entity.y <= 0 || entity.y >= screenHeight) {
+        return true;
+    }
+    return false;
+}
+
+//------------------------------------------------------------------------------------
+// Render bullets
+//------------------------------------------------------------------------------------
+void renderBullets(std::vector<Rectangle*>* bullets, int screenWidth, int screenHeight) {
+    std::vector<Rectangle*>::iterator iter = bullets->begin();
+    int i = 0;  // counter.
+
+    bullets->erase(std::remove_if(
+        bullets->begin(), 
+        bullets->end(),
+        [screenWidth, screenHeight](Rectangle* bullet)->bool {
+            Vector2 bulletPos = { bullet->x, bullet->y };
+            return outOfBounds(bulletPos, screenWidth, screenHeight);
+        }), 
+        bullets->end()
+	);
+
+    if (bullets->size() <= 0) {
+        return;
+    }
+
+    for (iter; iter != bullets->end(); ++iter, i++) {
+        auto bullet = *iter;
+        DrawRectangle(bullet->x, bullet->y, bullet->width, bullet->height, RED);
+        bullet->y -= BULLET_VEL;
+    }
+}
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -22,31 +81,48 @@ int main(void)
         (float)screenHeight / 2.0f,
     };
     
-    // Load Textures
+    // Load Textures + Create Entities
     //--------------------------------------------------------------------------------------
-    Texture2D shipTexture = LoadTexture("../resources/textures/ship2.png");
+    Texture2D shipTexture = LoadTexture("./resources/textures/ship.png");
     const int shipSize = 50;
+
+    // vector of bullets
+    std::vector<Rectangle*> bullets;
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
+	bool showHitboxes = false;
+    std::chrono::steady_clock::time_point lastShotTime;
+
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        bool fireWeapons = false;
         Vector2 oldShipPosition = shipPosition;
 
-        if (IsKeyDown(KEY_RIGHT)) shipPosition.x += 7.0f;
-        if (IsKeyDown(KEY_LEFT)) shipPosition.x -= 7.0f;
-        if (IsKeyDown(KEY_UP)) shipPosition.y -= 7.0f;
-        if (IsKeyDown(KEY_DOWN)) shipPosition.y += 7.0f;
-        if (IsKeyDown(KEY_SPACE)) fireWeapons = true;
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) shipPosition.x += 7.0f;
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) shipPosition.x -= 7.0f;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) shipPosition.y -= 7.0f;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) shipPosition.y += 7.0f;
+        if (IsKeyDown(KEY_H)) showHitboxes = !showHitboxes;
 
-        if (shipPosition.x <= 0 || shipPosition.x >= screenWidth
-            || shipPosition.y <= 0 || shipPosition.y >= screenHeight) {
+        if (outOfBounds(shipPosition, screenWidth, screenHeight)) {
             shipPosition = oldShipPosition;
+        }
+
+        if (IsKeyDown(KEY_SPACE)) {
+            auto now = std::chrono::high_resolution_clock::now();
+            auto timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastShotTime);
+            if ( timeElapsed.count() >= (float)1000 / BULLET_HEIGHT) {
+				fireWeapons(shipPosition, shipSize, &bullets);
+                lastShotTime = now;
+            }
+        }
+
+        // fire weapon(s)
+        if (IsKeyDown(KEY_SPACE)) {
         }
         //----------------------------------------------------------------------------------
 
@@ -57,20 +133,25 @@ int main(void)
         ClearBackground(BLACK);
 
         std::string shipPosText = fmt::format("X: {}, Y: {}", shipPosition.x, shipPosition.y);
+        std::string numBulletsText = fmt::format("# of Bullets: {}", bullets.size());
 
         DrawText("move the ship with arrow keys", 10, 10, 20, RAYWHITE);
         DrawText(shipPosText.c_str(), 10, 30, 20, RAYWHITE);
-
+        DrawText(numBulletsText.c_str(), 10, 50, 20, RAYWHITE);
+        
 
         Rectangle shipSourceRec = { 0.0f, 0.0f, (float)shipSize, (float)shipSize };
-        Rectangle shipDestRec = { shipPosition.x, shipPosition.x, shipSize, shipSize};
+        Rectangle shipDestRec = { shipPosition.x, shipPosition.y, shipSize, shipSize};
         Vector2 origin = { (float)shipSize/2, (float)shipSize/2 };
         Vector2 xOffset { (float)shipSize / 2, 0};
         Vector2 yOffset{ 0, -(float)shipSize / 2 };
 
+        // draw ship
         DrawTexturePro(shipTexture, shipSourceRec, shipDestRec, origin, (float)0, WHITE);
-        // DrawTexture(shipTexture, shipPosition.x, shipPosition.y, WHITE);
-        DrawRectangleLines(shipPosition.x - origin.x, shipPosition.y - origin.y, shipSize, shipSize, RED);
+        // draw bullets
+        renderBullets(&bullets, screenWidth, screenHeight);
+        // DrawTexture(shipTexture, shipPosition.x - origin.x, shipPosition.y - origin.y, WHITE);
+        if(showHitboxes) DrawRectangleLines(shipPosition.x - origin.x, shipPosition.y - origin.y, shipSize, shipSize, RED);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
