21c997cabb07cff4e82e280d784d97b331378b7d
Shr1ftyy <syeamtechdemon@example.com>
2023-09-26 20:21:21 +0930

    added shooting + enemy death sounds

--- src/SimpleEnemy.cpp
+++ src/SimpleEnemy.cpp
@@ -1,6 +1,6 @@
 #include "SimpleEnemy.h"
 #include "raymath.h"
 
-SimpleEnemy::SimpleEnemy(Texture2D _spriteSheet, Vector2 _src, Vector2 _indexingVec, int _numFrames, int _spriteFPS, Vector2 _textureDims, Vector2 _hitboxDims, Vector2 _origin, float _hp) :
-    Enemy(_spriteSheet, _src, _indexingVec, _numFrames, _spriteFPS, _textureDims, _hitboxDims, _origin, _hp)
+SimpleEnemy::SimpleEnemy(Texture2D _spriteSheet, Sound _deathSound, Vector2 _src, Vector2 _indexingVec, int _numFrames, int _spriteFPS, Vector2 _textureDims, Vector2 _hitboxDims, Vector2 _origin, float _hp) :
+    Enemy(_spriteSheet, _deathSound, _src, _indexingVec, _numFrames, _spriteFPS, _textureDims, _hitboxDims, _origin, _hp)
 { }
